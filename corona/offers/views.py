b098029522237f9bd672494eeaaa959c50c54baf
Johannes Lauinger <johannes@lauinger-it.de>
2020-03-16 20:31:42 +0100

    optimize imports

--- corona/offers/views.py
+++ corona/offers/views.py
@@ -4,29 +4,11 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import FormView, ListView, DeleteView, CreateView, DetailView, UpdateView
+from django.views.generic import FormView, ListView, DeleteView, CreateView, UpdateView
 
 from offers.forms import OfferSearchForm, UserForm, OfferForm, ProviderProfileForm
 from offers.helper import location_from_address
 from offers.models import Offer, ProviderProfile
-
-
-class OfferSearchView(FormView):
-    form_class = OfferSearchForm
-    template_name = 'offers/home.html'
-    success_url = ''
-
-    @staticmethod
-    def get_queryset(form):
-        location = location_from_address(form.cleaned_data['where'])
-        time = form.cleaned_data['when']
-
-        return Offer.offers_in_range_and_time(location, time)
-
-    def form_valid(self, form):
-        return render(self.request, 'offers/search_results.html', {
-            'object_list': self.get_queryset(form),
-        })
 
 
 class AccountRegistrationView(FormView):
@@ -112,3 +94,21 @@ class CreateOfferView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class OfferSearchView(FormView):
+    form_class = OfferSearchForm
+    template_name = 'offers/home.html'
+    success_url = ''
+
+    @staticmethod
+    def get_queryset(form):
+        location = location_from_address(form.cleaned_data['where'])
+        time = form.cleaned_data['when']
+
+        return Offer.offers_in_range_and_time(location, time)
+
+    def form_valid(self, form):
+        return render(self.request, 'offers/search_results.html', {
+            'object_list': self.get_queryset(form),
+        })
