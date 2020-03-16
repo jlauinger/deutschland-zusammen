b8251ec4b724f00f5ada20696f3b64e963018c66
Johannes Lauinger <johannes@lauinger-it.de>
2020-03-16 23:19:10 +0100

    add styles for profile details page

--- corona/offers/templatetags/template_tags.py
+++ corona/offers/templatetags/template_tags.py
@@ -6,3 +6,25 @@ register = template.Library()
 @register.filter
 def rounded_distance(distance):
     return int(round(distance.m, -2))
+
+
+@register.filter
+def get_zoom(radius):
+    if radius == 1000:
+        return 14
+    elif radius == 2000:
+        return 13
+    elif radius == 3000:
+        return 13
+    elif radius == 4000:
+        return 12
+    elif radius == 5000:
+        return 12
+    elif radius == 10000:
+        return 11
+    elif radius == 20000:
+        return 10
+    elif radius == 50000:
+        return 9
+    else:
+        return 13
