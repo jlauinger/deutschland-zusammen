1b4bf49a951b54224fa61045d7224726fa26bc72
Johannes Lauinger <johannes@lauinger-it.de>
2020-03-21 23:00:25 +0100

    add hostname to social graph meta tags

--- corona/offers/context_preprocessors.py
+++ corona/offers/context_preprocessors.py
@@ -5,3 +5,9 @@ def vapid_key(request):
     return {
         'vapid_key': settings.WEBPUSH_SETTINGS.get('VAPID_PUBLIC_KEY')
     }
+
+
+def hostname(request):
+    return {
+        'hostname': settings.HOST_NAME
+    }
